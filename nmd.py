0d55c8f23c50ef5a21345c17418c88114274d6b3
averykhoo <averykhoo@gmail.com>
2021-01-01 13:19:44 +0800

    finish faster algo (that might not always be faster...)

--- nmd.py
+++ nmd.py
@@ -34,7 +34,7 @@ def n_gram_emd(word_1: str, word_2: str, n: int = 2):
         if n_gram not in n_gram_locations_1:
             distance += len(locations)
         else:
-            print(n_gram, locations, n_gram_locations_1[n_gram])
+            # print(n_gram, locations, n_gram_locations_1[n_gram])
             distance += emd_1d(locations, n_gram_locations_1[n_gram])
 
     return distance, total
@@ -97,7 +97,105 @@ def emd_1d_fast(locations_x: List[float], locations_y: List[float]) -> float:
             break
 
     # remove any matching points in x and y
-    # todo: do this first
+    # todo: do this before removing endpoints
+    new_x = []
+    new_y = []
+    locations_x.reverse()
+    locations_y.reverse()
+    while locations_x and locations_y:
+        if locations_x[-1] < locations_y[-1]:
+            new_x.append(locations_x.pop(-1))
+        elif locations_x[-1] > locations_y[-1]:
+            new_y.append(locations_y.pop(-1))
+        else:
+            # discard duplicate
+            locations_x.pop(-1)
+            locations_y.pop(-1)
+    if locations_x:
+        locations_x.reverse()
+        new_x.extend(locations_x)
+    if locations_y:
+        locations_y.reverse()
+        new_y.extend(locations_y)
+    locations_x = new_x
+    locations_y = new_y
+
+    # another chance to early exit
+    if len(locations_y) == 0:
+        return acc + len(locations_x)
+    if len(locations_y) == 1:
+        return acc + min(abs(x - locations_y[0]) for x in locations_x) + len(locations_x) - 1
+
+    # there shouldn't be any duplicates across both lists now
+    assert len(locations_x) + len(locations_y) == len(set(locations_x + locations_y))
+
+    # enumerate the options instead of recursing
+    # todo: actually build the bipartite graph to exclude impossible match options?
+    acc += len(locations_x) - len(locations_y)
+    min_cost = len(locations_y)
+    for x_combination in itertools.combinations(locations_x, len(locations_y)):
+        min_cost = min(min_cost, sum(abs(x - y) for x, y in zip(x_combination, locations_y)))
+    return acc + min_cost
+
+
+def emd_1d_faster(locations_x: List[float], locations_y: List[float]) -> float:
+    """
+    distance needed to move
+    todo: optimize worst case
+    """
+
+    # all inputs must be in the unit interval
+    assert all(0 <= x <= 1 for x in locations_x)
+    assert all(0 <= x <= 1 for x in locations_y)
+
+    # in our use case, there should be no duplicates in each list
+    assert len(locations_x) == len(set(locations_x))
+    assert len(locations_y) == len(set(locations_y))
+
+    # locations_1 will be the longer list
+    if len(locations_x) < len(locations_y):
+        locations_x, locations_y = locations_y, locations_x
+
+    # empty list, so just count the l1 items and exit early
+    if len(locations_y) == 0:
+        return len(locations_x)
+
+    # only one item, so take min distance and count the rest of the l1 items
+    if len(locations_y) == 1:
+        return min(abs(l1 - locations_y[0]) for l1 in locations_x) + len(locations_x) - 1
+
+    # make a COPY of the list, sorted in reverse (descending order)
+    # we'll be modifying in-place later, and we don't want to update the input
+    locations_x = sorted(locations_x, reverse=True)
+    locations_y = sorted(locations_y, reverse=True)
+
+    # accumulated distance as we simplify the problem
+    acc = 0
+
+    # greedy-match constrained points with only one possible match (at the smaller end of locations_y)
+    while locations_y and locations_x:
+        if locations_y[-1] <= locations_x[-1]:
+            acc += locations_x.pop(-1) - locations_y.pop(-1)
+        elif len(locations_x) >= 2 and (locations_y[-1] - locations_x[-1]) <= (locations_x[-2] - locations_y[-1]):
+            acc += locations_y.pop(-1) - locations_x.pop(-1)
+        else:
+            break
+
+    # reverse both lists IN PLACE, so now they are sorted in ascending order
+    locations_x.reverse()
+    locations_y.reverse()
+
+    # greedy-match constrained points with only one possible match (at the larger end of locations_y)
+    while locations_y and locations_x:
+        if locations_y[-1] >= locations_x[-1]:
+            acc += locations_y.pop(-1) - locations_x.pop(-1)
+        elif len(locations_x) >= 2 and (locations_x[-1] - locations_y[-1]) <= (locations_y[-1] - locations_x[-2]):
+            acc += locations_x.pop(-1) - locations_y.pop(-1)
+        else:
+            break
+
+    # remove any matching points in x and y
+    # todo: do this before removing endpoints
     new_x = []
     new_y = []
     locations_x.reverse()
@@ -130,15 +228,15 @@ def emd_1d_fast(locations_x: List[float], locations_y: List[float]) -> float:
     assert len(locations_x) + len(locations_y) == len(set(locations_x + locations_y))
 
     # merge the lists for now to find sets of possibly paired points without actually building a bipartite graph
-    locations = sorted([(loc, 'x') for loc in locations_x] + [(loc, 'y') for loc in locations_y])
-    pair_ranges = []
+    locations = sorted([(loc, False) for loc in locations_x] + [(loc, True) for loc in locations_y])
+    component_ranges = []
     n = 0
     current_left = None
     current_right = None
 
     # get ranges of forward alignments
-    for idx, (loc, xy) in enumerate(locations):
-        if xy == 'x':
+    for idx, (loc, is_y) in enumerate(locations):
+        if not is_y:
             n += 1
         if n:
             if current_left is None:
@@ -146,23 +244,21 @@ def emd_1d_fast(locations_x: List[float], locations_y: List[float]) -> float:
                 current_right = idx
             else:
                 current_right = idx
-        if xy == 'y':
+        if is_y:
             n -= 1
         if not n:
-            pair_ranges.append((current_left, 'left'))
-            pair_ranges.append((current_right, 'right'))
+            component_ranges.append((current_left, current_right))
             current_left = None
             current_right = None
     if current_left is not None:
-        pair_ranges.append((current_left, 'left'))
-        pair_ranges.append((current_right, 'right'))
+        component_ranges.append((current_left, current_right))
         current_left = None
         current_right = None
 
     # get ranges of backward alignments
     for idx in range(len(locations) - 1, -1, -1):
-        loc, xy = locations[idx]
-        if xy == 'x':
+        loc, is_y = locations[idx]
+        if not is_y:
             n += 1
         if n:
             if current_right is None:
@@ -170,50 +266,55 @@ def emd_1d_fast(locations_x: List[float], locations_y: List[float]) -> float:
                 current_left = idx
             else:
                 current_left = idx
-        if xy == 'y':
+        if is_y:
             n -= 1
         if not n:
-            pair_ranges.append((current_right, 'right'))
-            pair_ranges.append((current_left, 'left'))
+            component_ranges.append((current_left, current_right))
             current_right = None
             current_left = None
     if current_right is not None:
-        pair_ranges.append((current_right, 'right'))
-        pair_ranges.append((current_left, 'left'))
+        component_ranges.append((current_left, current_right))
 
-    # merge ranges
-    pair_ranges = sorted(pair_ranges)
-    combined_ranges = []
-    n_open = 0
-    for loc, lr in pair_ranges:
-        if lr == 'left':
-            if not n_open:
-                combined_ranges.append((loc, lr))
-            n_open += 1
-
-        else:
-            n_open -= 1
-            if not n_open:
-                combined_ranges.append((loc, lr))
-
-    # todo: build the bipartite graph
-    # backward and forward pass
-
-    # todo: split into connected components
-    # this will remove all unmatchable points from the graph
+    # merge ranges to get the sets of connected components
     # [x1 y1 x2 x3 x4 y2 x3] ==> [x1 y1 x2], [x4 y2 x5] (x3 can never be matched)
+    component_ranges = sorted(component_ranges, reverse=True)
+    last_seen = -1
+    while component_ranges:
+        left, right = component_ranges.pop(-1)
+        while component_ranges and component_ranges[-1][0] < right:
+            _, right = component_ranges.pop(-1)
 
-    # todo: try to greedy-match unshared points for each component
-    # [x1 y1 ... x2 ...]       ==> if x1y1 < y1x2, then y1 -> x1
-    # [... x3 x4 y1 x5 x6 ...] ==> y1 can only match x4 or x5 (assuming there are no y-chains)
-    # if it succeeds, then remove the component
+        # count unmatched points since last seen
+        if left > last_seen + 1:
+            acc += left - last_seen - 1  # count unmatchable points
 
-    # enumerate the options instead of recursing
-    acc += len(locations_x) - len(locations_y)
-    min_cost = len(locations_y)
-    for x_combination in itertools.combinations(locations_x, len(locations_y)):
-        min_cost = min(min_cost, sum(abs(x - y) for x, y in zip(x_combination, locations_y)))
-    return acc + min_cost
+        # split into x and y lists again
+        connected_x = [idx for idx, is_y in locations[left:right + 1] if not is_y]
+        connected_y = [idx for idx, is_y in locations[left:right + 1] if is_y]
+
+        # todo: greedy match endpoints (again)?
+
+        # todo: try to greedy-match unshared points (must match all points in this component)?
+        # [x1 y1 ... x2 ...]       ==> if x1y1 < y1x2, then y1 -> x1
+        # [... x3 x4 y1 x5 x6 ...] ==> y1 can only match x4 or x5 (assuming there are no y-chains)
+        # if it succeeds, then remove the component
+
+        # enumerate the options instead of recursing
+        # todo: actually build the bipartite graph to exclude impossible match options?
+        acc += len(connected_x) - len(connected_y)
+        min_cost = len(connected_y)
+        for x_combination in itertools.combinations(connected_x, len(connected_y)):
+            min_cost = min(min_cost, sum(abs(x - y) for x, y in zip(x_combination, connected_y)))
+        acc += min_cost
+
+        # update last seen
+        last_seen = right
+
+    # count unmatched points after last seen
+    if len(locations) > last_seen + 1:
+        acc += len(locations) - last_seen - 1
+
+    return acc
 
 
 def emd_1d_slow(locations_x: List[float], locations_y: List[float]) -> float:
@@ -227,7 +328,275 @@ def emd_1d_slow(locations_x: List[float], locations_y: List[float]) -> float:
 
 
 def emd_1d(locations_x: List[float], locations_y: List[float]) -> float:
-    answer_1 = emd_1d_slow(locations_x, locations_y)
+    # answer_1 = emd_1d_slow(locations_x, locations_y)
+    answer_1 = emd_1d_faster(locations_x, locations_y)
     answer_2 = emd_1d_fast(locations_x, locations_y)
     assert abs(answer_1 - answer_2) < 0.00001, (answer_2, answer_1)
     return answer_2
+
+
+if __name__ == '__main__':
+    a = [
+        'Schwartzenegger',
+        'Schwarzeneger',
+        'Schwarzenager',
+        'Schwartzenager',
+        'Schwartzeneger',
+        'Schwarzeneggar',
+        'Schwarzenneger',
+        'Swartzenegger',
+        'Swarzenegger',
+        'Schwarzenagger',
+        'Schwarznegger',
+        'Swartzenager',
+        'Schwarzanegger',
+        'Shwarzenegger',
+        'Schwartzenagger',
+        'Swartzeneger',
+        'Schwartznegger',
+        'Schwarzenegar',
+        'Shwartzenegger',
+        'Schwarzennegger',
+        'Schwarzennager',
+        'Schwartzanegger',
+        'Schwartzenneger',
+        'Schwarzanager',
+        'Schwarzengger',
+        'Schwarzennegar',
+        'Shwartzeneger',
+        'Schwartzeneggar',
+        'Schwarzneger',
+        'Schwarzneggar',
+        'Schwartzenegar',
+        'Schwartzneger',
+        'Schwazenegger',
+        'Shwartzenager',
+        'Swartzanegger',
+        'Swarzeneger',
+        'Swarzeneggar',
+        'Schwarenegger',
+        'Schwartzennager',
+        'Schwartzneggar',
+        'Shwarzeneger',
+        'Swartzeneggar',
+        'Swartznegger',
+        'Swarzenager',
+        'Swarzenagger',
+        'Scharzenegger',
+        'Schwarnegger',
+        'Schwartnegger',
+        'Schwartzanager',
+        'Schwartzaneger',
+        'Schwartzinager',
+        'Schwarzzenager',
+        'Shwarzenager',
+        'Swartzenagger',
+        'Swartzineger',
+        'Scharzeneger',
+        'Schwarnzenegger',
+        'Schwartenager',
+        'Schwartenegar',
+        'Schwarteneger',
+        'Schwartnegar',
+        'Schwartzanegar',
+        'Schwartzenger',
+        'Schwartzenggar',
+        'Schwartzineger',
+        'Schwartznager',
+        'Schwarzaneger',
+        'Schwarzaneggar',
+        'Schwarzanger',
+        'Schwarzenaeger',
+        'Schwarzeniger',
+        'Schwarzinager',
+        'Schwarznager',
+        'Schwarztenegger',
+        'Schwarzzeneger',
+        'Schwarzzenegger',
+        'Schwazenager',
+        'Schwazeneger',
+        'Scwartzenegger',
+        'Scwarzenegger',
+        'Shwartznegger',
+        'Shwarzenegar',
+        'Swarteneger',
+        'Swartzanager',
+        'Swartznager',
+        'Swartzneger',
+        'Swarzanegger',
+        'Swarzennager',
+        'Swarzenneger',
+        'Swazeneger',
+        'Schartzenager',
+        'Schartzennager',
+        'Schartznager',
+        'Scharwzeneger',
+        'Scharzenager',
+        'Schawarzneneger',
+        'Schawrknegger',
+        'Schazenegger',
+        'Schneckenger',
+        'Schrarznegger',
+        'Schrawzenneger',
+        'Schrwazeneggar',
+        'Schrwazenegger',
+        'Schrwtzanagger',
+        'Schsargdneger',
+        'Schwaranagger',
+        'Schwararzenegger',
+        'Schwarezenegger',
+        'Schwarganzer',
+        'Schwarnznegar',
+        'Schwarsanegger',
+        'Schwarsenagger',
+        'Schwarsnegger',
+        'Schwartaneger',
+        'Schwartenagger',
+        'Schwartenegger',
+        'Schwartenneger',
+        'Schwarterneger',
+        'Schwartineger',
+        'Schwartnager',
+        'Schwartnehar',
+        'Schwartsaneger',
+        'Schwartsinager',
+        'Schwartzaneggar',
+        'Schwartzanger',
+        'Schwartzeiojaweofjaweneger',
+        'Schwartzenagar',
+        'Schwartzenegget',
+        'Schwartzeneiger',
+        'Schwartzengar',
+        'Schwartzenkangaroo',
+        'Schwartzennegar',
+        'Schwartzinagger',
+        'Schwartzinegar',
+        'Schwartziniger',
+        'Schwartznagger',
+        'Schwartznegar',
+        'Schwarz',
+        'Schwarzamegger',
+        'Schwarzanagger',
+        'Schwarzatwizzler',
+        'Schwarzeggar',
+        'Schwarzegger',
+        'Schwarzenaega',
+        'Schwarzenagher',
+        'Schwarzeneeger',
+        'Schwarzenegor',
+        'Schwarzenenergy',
+        'Schwarzengeggar',
+        'Schwarzgenar',
+        'Schwarzinagger',
+        'Schwarzineggar',
+        'Schwarztenegar',
+        'Schwarzzanager',
+        'Schwatzeneggar',
+        'Schwatzenneger',
+        'Schwazenaeger',
+        'Schwazenegrr',
+        'Schwazerneger',
+        'Schwazinager',
+        'Schwaznagger',
+        'Schwazneger',
+        'Schwaznnager',
+        'Schwazzeneger',
+        'Schwazzenger',
+        'Schwazzinager',
+        'Schzwarnegger',
+        'Scwarrzenegger',
+        'Scwarzenager',
+        'Scwarzeneggar',
+        'Scwarzenneger',
+        'Scwharzanegger',
+        'Scwharzeneggar',
+        'Shwarsneger',
+        'Shwartaneger',
+        'Shwarteneger',
+        'Shwartinznegar',
+        'Shwartnierger',
+        'Shwartsnagger',
+        'Shwartzanager',
+        'Shwartzanegar',
+        'Shwartzaneger',
+        'Shwartzanegger',
+        'Shwartzenagor',
+        'Shwartzeneggar',
+        'Shwartzengar',
+        'Shwartzennegar',
+        'Shwartzganeger',
+        'Shwartznager',
+        'Shwartzneger',
+        'Shwarzanegger',
+        'Shwarzenagger',
+        'Shwarzenneger',
+        'Shwarznager',
+        'Shwaztsinager',
+        'Swarchneger',
+        'Swarchzinager',
+        'Swarchznegger',
+        'Swartenager',
+        'Swartenegger',
+        'Swartenzager',
+        'Swartiznager',
+        'Swartschenager',
+        'Swartseneger',
+        'Swartseneggar',
+        'Swartsenenger',
+        'Swartshanaiger',
+        'Swarttenegger',
+        'Swartz.',
+        'Swartzanagger',
+        'Swartzaneger',
+        'Swartzanegga',
+        'Swartzeigner',
+        'Swartzenagar',
+        'Swartzeneagar',
+        'Swartzenegar',
+        'Swartzenegher',
+        'Swartzengger',
+        'Swartzennager',
+        'Swartzennegar',
+        'Swartzenneger',
+        'Swartzerniger',
+        'Swartzinager',
+        'Swartzineggar',
+        'Swartznagger',
+        'Swartznegar',
+        'Swartzneggar',
+        'Swarzenaeger',
+        'Swarzenaggar',
+        'Swarzenaider',
+        'Swarzengger',
+        'Swarzneger',
+        'Swarznegger',
+        'Swarzshnegger',
+        'Swarzzeneggar',
+        'Swarzzenegger',
+        'Swatgnezzer',
+        'Swatz..',
+        'Swatzinagger',
+        'Swazenegger',
+        'Swazernager',
+        'Swchwartzignegeridknga',
+        'Swchwazaneger',
+        'Swertizager',
+        'Swertzeneggar',
+        'Swhartznegar',
+        'Switzenagger',
+        'Swiztinager',
+        'Swuartzenegar',
+        'Schwartzanagger',
+        'Schwartzennnnnnn',
+        'Schwarzenger',
+        'Swartasenegger',
+        'Swazenegar',
+    ]
+    b = 'Schwarzenegger'
+
+    print(n_gram_emd('banana', 'bababanananananananana'))
+    print(n_gram_emd('banana', 'bababanananananananananna'))
+    print(n_gram_emd('banana', 'nanananananabababa'))
+    print(n_gram_emd('banana', 'bababananananananananannanananananananana'))
+    print(n_gram_emd('banana', 'bababananananananananannananananananananananananananannanananananananana'))
+    print(n_gram_emd('bananabababanana', 'bababananananananananannananananananananananananananannananabanananananana'))

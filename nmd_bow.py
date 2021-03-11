3ebb0ab8b63ec45c12f3f242dd933c962382bb8e
Avery Khoo <averykhoo@gmail.com>
2021-03-11 11:09:05 +0800

    handle n > len(word)

--- nmd_bow.py
+++ nmd_bow.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from typing import Iterable
 from typing import Union
 
@@ -23,20 +22,26 @@ def bow_ngram_movers_distance(bag_of_words_1: Union[str, Iterable[str]],
     """
 
     # convert to list
-    bag_of_words_1 = list(bag_of_words_1)  # columns
-    bag_of_words_2 = list(bag_of_words_2)  # rows
+    bag_of_words_1 = list(bag_of_words_1)  # rows
+    bag_of_words_2 = list(bag_of_words_2)  # columns
 
     # optimize cost matrix using EMD
-    costs = [[ngram_movers_distance(word_1, word_2, normalize=True)
-              for word_1 in bag_of_words_1]
-             for word_2 in bag_of_words_2]
-
+    costs = []
+    for word_1 in bag_of_words_1:
+        row = []
+        for word_2 in bag_of_words_2:
+            try:
+                row.append(ngram_movers_distance(word_1, word_2, n=n, normalize=True))
+            except ZeroDivisionError:
+                row.append(int(word_1 != word_2))
+        costs.append(row)
     row_idxs, col_idxs = scipy.optimize.linear_sum_assignment(costs)  # 1D equivalent of EMD
 
     # sum
     out = abs(len(bag_of_words_1) - len(bag_of_words_2))
     for row_idx, col_idx in zip(row_idxs, col_idxs):
         out += costs[row_idx][col_idx]
+        # print(bag_of_words_2[col_idx], bag_of_words_1[row_idx], costs[row_idx][col_idx])
 
     return out
 
@@ -52,17 +57,24 @@ if __name__ == '__main__':
     for ref_line, hyp_line in zip(ref_lines, hyp_lines):
         ref_tokens = list(unicode_tokenize(ref_line.casefold(), words_only=True, merge_apostrophe_word=True))
         hyp_tokens = list(unicode_tokenize(hyp_line.casefold(), words_only=True, merge_apostrophe_word=True))
-        scores_bow.append(bow_ngram_movers_distance(ref_tokens, hyp_tokens) / max(len(ref_tokens), len(hyp_tokens)))
-        scores_nmd.append(ngram_movers_distance(' '.join(ref_tokens), ' '.join(hyp_tokens), normalize=True))
+        scores_bow.append(bow_ngram_movers_distance(ref_tokens, hyp_tokens, 4) / max(len(ref_tokens), len(hyp_tokens)))
+        scores_nmd.append(ngram_movers_distance(' '.join(ref_tokens), ' '.join(hyp_tokens), 4, normalize=True))
         print(' '.join(ref_tokens))
         print(' '.join(hyp_tokens))
-        print(bow_ngram_movers_distance(ref_tokens, hyp_tokens) / max(len(ref_tokens), len(hyp_tokens)))
-        print(ngram_movers_distance(' '.join(ref_tokens), ' '.join(hyp_tokens), normalize=True))
-
+        print(scores_bow[-1])
+        print(scores_nmd[-1])
 
     from matplotlib import pyplot as plt
+
     plt.scatter(scores_bow, scores_nmd, marker='.')
     plt.show()
-    scores_diff = [a-b for a,b in zip(scores_bow, scores_nmd)]
-    print(min(zip(scores_diff,scores_bow,scores_nmd,ref_lines,hyp_lines)))
-    print(max(zip(scores_diff,scores_bow,scores_nmd,ref_lines,hyp_lines)))
+    scores_diff = [a - b for a, b in zip(scores_bow, scores_nmd)]
+    tmp = sorted(zip(scores_diff, scores_bow, scores_nmd, ref_lines, hyp_lines))
+    print(tmp[0])
+    print(tmp[1])
+    print(tmp[2])
+    print(tmp[3])
+    print(tmp[-1])
+    print(tmp[-2])
+    print(tmp[-3])
+    print(tmp[-4])

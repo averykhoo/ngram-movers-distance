9d5afe14d92af27178c8205efccb7f83e2108652
Avery Khoo <averykhoo@gmail.com>
2023-07-13 14:23:45 +0800

    bump version

--- nmd/__init__.py
+++ nmd/__init__.py
@@ -2,12 +2,12 @@
 A string similarity measure based on the Earth Mover's Distance
 """
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 from nmd.nmd import ngram_movers_distance
 from nmd.nmd_index import WordList
 
-__all__ = [
-    ngram_movers_distance,
-    WordList,
-]
+__all__ = (
+    'ngram_movers_distance',
+    'WordList',
+)
